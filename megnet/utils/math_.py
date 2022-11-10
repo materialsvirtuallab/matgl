e96d246dae53b11658fbbdaf2dfafc75134856a3
Shyue Ping Ong <sp@ong.ai>
2022-11-10 08:38:56 -0800

    Use the more efficient numpy binary format.

--- megnet/utils/math_.py
+++ megnet/utils/math_.py
@@ -14,4 +14,4 @@ CWD = os.path.dirname(os.path.abspath(__file__))
 Precomputed Spherical Bessel function roots in a 2D array with dimension [128, 128]. The n-th (0-based index） root of
 order l Spherical Bessel function is the `[l, n]` entry.
 """
-SPHERICAL_BESSEL_ROOTS = np.loadtxt(os.path.join(CWD, "sb_roots.txt"))
+SPHERICAL_BESSEL_ROOTS = np.load(os.path.join(CWD, "sb_roots.npy"))
