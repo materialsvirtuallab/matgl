7894937c3083fa76250100292947c45fe09d5662
ko tsz wai <kenko@kos-MacBook-Pro.local>
2022-12-07 10:31:15 -0800

    Missing docstring is added

--- megnet/dataloader/MEGNetDataset.py
+++ megnet/dataloader/MEGNetDataset.py
@@ -1,3 +1,6 @@
+"""
+Tools to construct a dataloader for DGL grphs
+"""
 import os
 
 import dgl
@@ -10,7 +13,7 @@ from tqdm import trange
 
 def _collate_fn(batch):
     """
-    Merge a list of samples to form a mini batch for dgl graphs
+    Merge a list of dgl graphs to form a batch
     """
     graphs, labels = map(list, zip(*batch))
     g = dgl.batch(graphs)
@@ -20,6 +23,7 @@ def _collate_fn(batch):
 
 def MEGNetDataLoader(train_data, val_data, test_data, collate_fn, batch_size, num_workers):
     """
+    Dataloader for MEGNet training
     Args:
     train_data: training data
     val_data: validation data
@@ -57,14 +61,17 @@ def MEGNetDataLoader(train_data, val_data, test_data, collate_fn, batch_size, nu
 
 class MEGNetDataset(DGLDataset):
     """
-    Args:
-    structures: Pymatgen strutcure
-    labels: property values
-    label: label name
-    cry_graph: Pmg2Graph
+    Create a dataset including dgl graphs
     """
 
     def __init__(self, structures, labels, label_name, cry_graph):
+        """
+        Args:
+        structures: Pymatgen strutcure
+        labels: property values
+        label: label name
+        cry_graph: Pmg2Graph
+        """
         self.cry_graph = cry_graph
         self.structures = structures
         self.labels = torch.FloatTensor(labels)
@@ -73,13 +80,22 @@ class MEGNetDataset(DGLDataset):
         super().__init__(name="MEGNetDataset")
 
     def has_cache(self):
+        """
+        Check if the dgl_graph.bin exists or not
+        """
         graph_path = str(self.path) + "/dgl_graph.bin"
         return os.path.exists(graph_path)
 
     def process(self):
+        """
+        Call _load_graph to convert Pymatgen structure into dgl graphs
+        """
         self.graphs = self._load_graph()
 
     def _load_graph(self):
+        """
+        Function for converting Pymatgen structure into dgl graphs
+        """
         num_graphs = self.labels.shape[0]
         graphs = []
 
@@ -90,17 +106,29 @@ class MEGNetDataset(DGLDataset):
         return graphs
 
     def save(self):
+        """
+        Save dgl graphs
+        """
         graph_path = str(self.path) + "/dgl_graph.bin"
         labels_with_key = {self.label_name: self.labels}
         save_graphs(graph_path, self.graphs, labels_with_key)
 
     def load(self):
+        """
+        Load dgl graphs
+        """
         graph_path = str(self.path) + "/dgl_graph.bin"
         self.graphs, label_dict = load_graphs(graph_path)
         self.label = torch.stack([label_dict[key] for key in self.label_keys], dim=1)
 
     def __getitem__(self, idx):
+        """
+        Get graph and label with idx
+        """
         return self.graphs[idx], self.labels[idx]
 
     def __len__(self):
+        """
+        Get size of dataset
+        """
         return len(self.graphs)
