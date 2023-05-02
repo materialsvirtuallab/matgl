13182ca31bab432c0d318dd02849c79a57490ed4
Shyue Ping Ong <sp@ong.ai>
2023-05-02 12:06:56 -0700

    Use cached models by default. Allow redownload if necessary.

--- matgl/utils/data.py
+++ matgl/utils/data.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+from pathlib import Path
 
 import requests
 
@@ -12,13 +13,15 @@ class ModelSource:
     Defines a local or remote source for models.
     """
 
-    def __init__(self, uri, use_cache=True):
+    def __init__(self, uri: str, use_cache: bool = True, force_download: bool = False):
         """
 
         Args:
             uri: Uniform resource identifier.
             use_cache: By default, downloaded models are saved at $HOME/.matgl/models. If False, models will be
                 downloaded to current working directory.
+            force_download: To speed up access, a model with the same name in the cache location will be used if
+                present. If you want to force a re-download, set this to True.
         """
         self.uri = uri
         self.model_name = uri.split("/")[-1]
@@ -27,15 +30,14 @@ class ModelSource:
                 os.makedirs(PRETRAINED_MODELS_PATH)
             self.local_path = PRETRAINED_MODELS_PATH / self.model_name
         else:
-            self.local_path = self.model_name
-        self._download()
+            self.local_path = Path(self.model_name)
+        if (not self.local_path.exists()) or force_download:
+            self._download()
 
     def _download(self):
         r = requests.get(self.uri, allow_redirects=True)
-
         with open(self.local_path, "wb") as f:
             f.write(r.content)
-        return self.local_path
 
     def __enter__(self):
         """
@@ -49,6 +51,7 @@ class ModelSource:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         """
+        Exit the with context.
 
         Args:
             exc_type:
