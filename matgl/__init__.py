ec3b93a2b2bda8540034ab3df8a5e9f3edf8da61
Shyue Ping Ong <shyuep@gmail.com>
2023-06-08 14:42:29 -0700

    Increment version.

--- matgl/__init__.py
+++ matgl/__init__.py
@@ -3,6 +3,6 @@ This package implements graph networks for materials science.
 """
 from __future__ import annotations
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 from .utils.io import load_model
