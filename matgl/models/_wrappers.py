025ff4e0bc8f2c7e427555326ed35e52828a2981
Shyue Ping Ong <shyuep@gmail.com>
2023-06-08 13:30:50 -0700

    Simplified TransformedTargetModel.

--- matgl/models/_wrappers.py
+++ matgl/models/_wrappers.py
@@ -3,14 +3,9 @@ Implementations of pseudomodels that wraps other models.
 """
 from __future__ import annotations
 
-import torch
-from pymatgen.core import Structure
 from torch import nn
 
 from matgl.data.transformer import Transformer
-from matgl.ext.pymatgen import Structure2Graph
-from matgl.graph.compute import compute_pair_vector_and_distance
-from matgl.graph.converters import GraphConverter
 from matgl.utils.io import IOMixIn
 
 
@@ -46,30 +41,15 @@ class TransformedTargetModel(nn.Module, IOMixIn):
     def __repr__(self):
         return f"TransformedTargetModel:\nModel: {self.model.__repr()}\nTransformer: {self.transformer.__repr__()}"
 
-    def predict_structure(
-        self,
-        structure: Structure,
-        state_feats: torch.tensor | None = None,
-        graph_converter: GraphConverter | None = None,
-    ):
+    def predict_structure(self, *args, **kwargs):
         """
-        Convenience method to directly predict property from structure.
+        Pass through to parent model.predict_structure with inverse transform.
 
         Args:
-            structure (Structure): Pymatgen structure
-            state_feats (torch.tensor): graph attributes
-            graph_converter: Object that implements a get_graph_from_structure.
-            data_mean: Mean of the data. Used when the original data has been scaled.
-            data_std: Std of the data. Used when the original data has been scaled.
+            *args: Pass-through to self.model.predict_structure.
+            **kwargs: Pass-through to self.model.predict_structure.
 
         Returns:
-            output (torch.tensor): output property
+            Transformed answer.
         """
-        if graph_converter is None:
-            graph_converter = Structure2Graph(element_types=self.model.element_types, cutoff=self.model.cutoff)
-        g, state_feats_default = graph_converter.get_graph(structure)
-        if state_feats is None:
-            state_feats = torch.tensor(state_feats_default)
-        bond_vec, bond_dist = compute_pair_vector_and_distance(g)
-        g.edata["edge_attr"] = self.model.bond_expansion(bond_dist)
-        return self(g, g.edata["edge_attr"], g.ndata["node_type"], state_feats).detach()
+        return self.transformer.inverse_transform(self.model.predict_structure(*args, **kwargs))
