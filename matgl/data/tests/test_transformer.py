47ce58c5834fb2520cbcf692d4a67ca84e5056da
Shyue Ping Ong <shyuep@gmail.com>
2023-06-08 08:31:12 -0700

    Set tol for tests.

--- matgl/data/tests/test_transformer.py
+++ matgl/data/tests/test_transformer.py
@@ -12,6 +12,6 @@ class TestNormalizer:
         transformer = Normalizer.from_data(data)
         scaled = transformer.transform(data)
 
-        assert float(scaled.mean()), pytest.approx(0)
+        assert float(scaled.mean()), pytest.approx(0, abs=1e-6)
         inverse = transformer.inverse_transform(scaled)
-        assert inverse == pytest.approx(data)
+        assert inverse == pytest.approx(data, abs=1e-6)
