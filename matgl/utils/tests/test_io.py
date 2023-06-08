4fb56a35b2c7583ad62ff62376c6ef0848c99358
Shyue Ping Ong <shyuep@gmail.com>
2023-06-08 10:53:57 -0700

    Fix tests.

--- matgl/utils/tests/test_io.py
+++ matgl/utils/tests/test_io.py
@@ -14,8 +14,8 @@ class ModelSourceTestCase(unittest.TestCase):
             "https://github.com/materialsvirtuallab/matgl/raw/main/pretrained_models/MEGNet-MP-2018.6.1-Eform/model.pt",
             cache_location=".",
         ) as s:
-            model = torch.load(s, map_location=torch.device("cpu"))
-            assert "nblocks" in model
+            d = torch.load(s, map_location=torch.device("cpu"))
+            assert "nblocks" in d["model"]["init_args"]
         try:  # cleanup
             shutil.rmtree("MEGNet-MP-2018.6.1-Eform")
         except FileNotFoundError:
