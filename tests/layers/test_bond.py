76d568529e0f85d8af86d96d195861ae6410fb54
Shyue Ping Ong <sp@ong.ai>
2023-06-15 08:33:02 -0700

    Fix bad test.

--- tests/layers/test_bond.py
+++ tests/layers/test_bond.py
@@ -2,24 +2,27 @@ from __future__ import annotations
 
 from pymatgen.core import Lattice, Molecule, Structure
 
-from matgl.ext.pymatgen import Structure2Graph, get_element_list
+from matgl.ext.pymatgen import Structure2Graph, Molecule2Graph, get_element_list
 from matgl.graph.compute import compute_pair_vector_and_distance
 from matgl.layers import BondExpansion
 
 
 class TestBondExpansion:
-    s = Structure(Lattice.cubic(4.0), ["Mo", "S"], [[0.01, 0.0, 0.0], [0.5, 0.5, 0.5]])
-    mol = Molecule(["C", "O"], [[0, 0, 0], [1.1, 0, 0]])
+    @classmethod
+    def setup_class(cls):
+        s = Structure(Lattice.cubic(4.0), ["Mo", "S"], [[0.01, 0.0, 0.0], [0.5, 0.5, 0.5]])
+        mol = Molecule(["C", "O"], [[0, 0, 0], [1.1, 0, 0]])
 
-    element_types = get_element_list([s])
-    p2g = Structure2Graph(element_types=element_types, cutoff=4.0)  # type: ignore
-    graph, state = p2g.get_graph(s)
-    g1 = graph
-    state1 = state
+        element_types = get_element_list([s])
+        p2g = Structure2Graph(element_types=element_types, cutoff=4.0)  # type: ignore
+        graph, state = p2g.get_graph(s)
+        cls.g1 = graph
+        cls.state1 = state
 
-    graph, state = p2g.get_graph(mol)
-    g2 = graph
-    state2 = state
+        m2g = Molecule2Graph(["C", "O"])
+        graph, state = m2g.get_graph(mol)
+        cls.g2 = graph
+        cls.state2 = state
 
     def test_gaussian(self):
         bond_expansion = BondExpansion(rbf_type="Gaussian", num_centers=10, initial=0.0, final=4.0, width=0.5)
