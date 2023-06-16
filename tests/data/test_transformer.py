beb2becbf6084ce73ca3a9b02c72358795a25f9a
Shyue Ping Ong <sp@ong.ai>
2023-06-16 15:10:54 -0700

    Test fix.

--- tests/data/test_transformer.py
+++ tests/data/test_transformer.py
@@ -6,7 +6,7 @@ import torch
 from matgl.data.transformer import LogTransformer, Normalizer
 
 
-def test_Normalizer(self):
+def test_Normalizer():
     data = torch.randn(100) * 10 + 1
     transformer = Normalizer.from_data(data)
     scaled = transformer.transform(data)
@@ -15,7 +15,7 @@ def test_Normalizer(self):
     assert inverse == pytest.approx(data, abs=1e-5)
 
 
-def test_LogTransformer(self):
+def test_LogTransformer():
     data = torch.rand(100) * 8 + 10
     transformer = LogTransformer()
     logdata = transformer.transform(data)
