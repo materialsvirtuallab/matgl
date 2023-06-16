f5ec54dd6ec2c09f651069986737d9f78436bc20
Shyue Ping Ong <sp@ong.ai>
2023-06-16 15:15:47 -0700

    One last fix.

--- tests/ext/test_ase.py
+++ tests/ext/test_ase.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from pymatgen.core.structure import Lattice, Structure
 from pymatgen.io.ase import AseAtomsAdaptor
 
 from matgl.apps.pes import Potential
@@ -10,16 +9,14 @@ from matgl.models import M3GNet
 
 
 class TestAseInterface:
-    s = Structure(Lattice.cubic(4.0), ["Mo", "S"], [[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]])
-    element_types = get_element_list([s])  # type: ignore
-    adaptor = AseAtomsAdaptor()
-    s_ase = adaptor.get_atoms(s)  # type: ignore
-
-    def test_M3GNetCalculator(self):
-        model = M3GNet(element_types=self.element_types, is_intensive=False)
+    def test_M3GNetCalculator(self, MoS):
+        element_types = get_element_list([MoS])  # type: ignore
+        adaptor = AseAtomsAdaptor()
+        s_ase = adaptor.get_atoms(MoS)  # type: ignore
+        model = M3GNet(element_types=element_types, is_intensive=False)
         ff = Potential(model=model)
         calc = M3GNetCalculator(potential=ff)
-        self.s_ase.set_calculator(calc)
+        s_ase.set_calculator(calc)
         assert [self.s_ase.get_potential_energy().size] == [1]
         assert list(self.s_ase.get_forces().shape) == [2, 3]
         assert list(self.s_ase.get_stress().shape) == [6]
