43e9ea6dc62b2e1c9013c801002db0527cf0b04b
Luis Barroso-Luque <lbluque@berkeley.edu>
2023-06-21 10:44:41 -0700

    TST: test_cosine pytest + torch

--- tests/utils/test_cutoff.py
+++ tests/utils/test_cutoff.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import numpy as np
 import torch
 from torch.testing import assert_close
 
@@ -8,16 +7,15 @@ from matgl.layers._basis import SphericalBesselFunction
 from matgl.utils.cutoff import cosine_cutoff, polynomial_cutoff
 
 
-class TestCutoff:
-    def test_cosine(self):
-        r = torch.linspace(1.0, 5.0, 11)
-        three_body_cutoff = 4.0
-        three_cutoff = cosine_cutoff(r, three_body_cutoff)
-        np.testing.assert_almost_equal(
-            three_cutoff.tolist(),
-            torch.tensor([0.8536, 0.7270, 0.5782, 0.4218, 0.2730, 0.1464, 0.0545, 0.0062, 0.0000, 0.0000, 0.0000]),
-            decimal=4,
-        )
+def test_cosine():
+    r = torch.linspace(1.0, 5.0, 11)
+    three_body_cutoff = 4.0
+    three_cutoff = cosine_cutoff(r, three_body_cutoff)
+    assert_close(
+        three_cutoff,
+        torch.tensor([0.8536, 0.7270, 0.5782, 0.4218, 0.2730, 0.1464, 0.0545, 0.0062, 0.0000, 0.0000, 0.0000]),
+        atol=1e-4, rtol=0.0
+    )
 
 
 def test_polymonial_cutoff():
