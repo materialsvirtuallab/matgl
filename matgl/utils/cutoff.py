f42bdfad132c08034943f4e5619ba304f6ae2a69
Luis Barroso-Luque <lbluque@berkeley.edu>
2023-06-21 10:43:08 -0700

    MAINT: generalize cutoff_polynomial and remove redundant SmoothPolyEnvelop

--- matgl/utils/cutoff.py
+++ matgl/utils/cutoff.py
@@ -7,17 +7,27 @@ from math import pi
 import torch
 
 
-def polynomial_cutoff(r, cutoff: float):
-    """Polynomial cutoff function
+def polynomial_cutoff(r: torch.tensor, cutoff: float, exponent: int = 3) -> torch.tensor:
+    """Envelope polynomial function that ensures a smooth cutoff.
+
+    Ensures first and second derivative vanish at cuttoff. As described in:
+        https://arxiv.org/abs/2003.03123
+
     Args:
         r (torch.tensor): radius distance tensor
         cutoff (float): cutoff distance.
+        exponent (int): minimum exponent of the polynomial. Default is 5.
+            The polynomial will include terms of order exponent, exponent + 1, exponent + 2.
 
-    Returns: polynomial cutoff functions
-
+    Returns: polynomial cutoff function
     """
+    a = -(exponent + 1) * (exponent + 2) / 2
+    b = exponent * (exponent + 2)
+    c = -exponent * (exponent + 1) / 2
     ratio = r / cutoff
-    return torch.where(r <= cutoff, 1 - 6 * ratio**5 + 15 * ratio**4 - 10 * ratio**3, 0.0)
+    poly_envelope = 1 + a * ratio**exponent + b * ratio**(exponent + 1) + c * ratio**(exponent + 2)
+
+    return torch.where(r <= cutoff, poly_envelope, 0.0)
 
 
 def cosine_cutoff(r: torch.Tensor, cutoff: float) -> torch.Tensor:
