afedda3e43355e42e7e8132e795b7cc883bca9a6
Ji Qi <qiji@Jis-MacBook-Air-7.local>
2023-06-25 16:35:29 -0400

    ruff

--- matgl/layers/__init__.py
+++ matgl/layers/__init__.py
@@ -3,13 +3,10 @@ from __future__ import annotations
 
 from matgl.layers._activations import SoftExponential, SoftPlus2
 from matgl.layers._atom_ref import AtomRef
-from matgl.layers._basis import (FourierExpansion, RadialBesselFunction,
-                                 SphericalBesselWithHarmonics)
+from matgl.layers._basis import FourierExpansion, RadialBesselFunction, SphericalBesselWithHarmonics
 from matgl.layers._bond import BondExpansion
 from matgl.layers._core import MLP, EdgeSet2Set, GatedMLP
 from matgl.layers._embedding import EmbeddingBlock
-from matgl.layers._graph_convolution import (M3GNetBlock, M3GNetGraphConv,
-                                             MEGNetBlock, MEGNetGraphConv)
-from matgl.layers._readout import (ReduceReadOut, Set2SetReadOut,
-                                   WeightedReadOut, WeightedReadOutPair)
+from matgl.layers._graph_convolution import M3GNetBlock, M3GNetGraphConv, MEGNetBlock, MEGNetGraphConv
+from matgl.layers._readout import ReduceReadOut, Set2SetReadOut, WeightedReadOut, WeightedReadOutPair
 from matgl.layers._three_body import ThreeBodyInteractions
