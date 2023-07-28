570d30af48b33d8ac03dbaec96f66af0df1cd20d
Shyue Ping Ong <sp@ong.ai>
2023-07-28 07:48:12 -0700

    Add an integratino test for the stochastic problems of the earlier M3Gnet form E models.

---
+++ tests/test_integration.py
@@ -0,0 +1,11 @@
+"""This is an integration test file that checks on pre-trained models to ensure they still work."""
+
+import matgl
+import pytest
+
+
+def test_form_e(LiFePO4):
+    model = matgl.load_model("M3GNet-MP-2018.6.1-Eform")
+    for i in range(3):
+        # This loop ensures there is no stochasticity in the prediction, a problem in v1 of the models.
+        assert model.predict_structure(LiFePO4) == pytest.approx(-2.5489, 3)
