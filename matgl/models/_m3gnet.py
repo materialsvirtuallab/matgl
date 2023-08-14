41ec7eff689a3526543247c5eaf405f8ebd23dff
Shyue Ping Ong <shyuep@gmail.com>
2023-08-14 11:59:00 -0700

    Remove all device calls.

--- matgl/models/_m3gnet.py
+++ matgl/models/_m3gnet.py
@@ -245,10 +245,7 @@ class M3GNet(nn.Module, IOMixIn):
                 l_g.ndata["pbc_offset"] = g.edata["pbc_offset"][valid_three_body]
             else:
                 three_body_id = torch.unique(torch.concatenate(l_g.edges()))
-                if three_body_id.numel() > 0:
-                    max_three_body_id = torch.max(three_body_id) + 1
-                else:
-                    max_three_body_id = torch.tensor(0, dtype=torch.int64, device=g.device)
+                max_three_body_id = torch.max(three_body_id) + 1 if three_body_id.numel() > 0 else 0
                 l_g.ndata["bond_vec"] = g.edata["bond_vec"][:max_three_body_id]
                 l_g.ndata["bond_dist"] = g.edata["bond_dist"][:max_three_body_id]
                 l_g.ndata["pbc_offset"] = g.edata["pbc_offset"][:max_three_body_id]
