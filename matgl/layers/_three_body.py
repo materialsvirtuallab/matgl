a2bfd96e06f8f72c011a921ff3a2670f23e05b9a
Shyue Ping Ong <sp@ong.ai>
2023-08-23 05:03:52 -0700

    Fix refactoring deprecation.

--- matgl/layers/_three_body.py
+++ matgl/layers/_three_body.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import numpy as np
 import torch
 from torch import nn
 
@@ -97,11 +98,11 @@ def combine_sbf_shf(sbf, shf, max_n: int, max_l: int, use_phi: bool):
         block_size = [1] * max_l
     else:
         # [1, 1, 1, ..., 1, 3, 3, 3, ..., 3, ...]
-        repeats_sbf = torch.repeat(2 * torch.arange(max_l) + 1, repeats=max_n)
+        repeats_sbf = np.repeat(2 * torch.arange(max_l) + 1, repeats=max_n)
         # tf.repeat(2 * tf.range(max_l) + 1, repeats=max_n)
         block_size = 2 * torch.arange(max_l) + 1  # type: ignore
         # 2 * tf.range(max_l) + 1
-    expanded_sbf = torch.repeat_interleave(sbf, repeats_sbf, 1)
+    expanded_sbf = np.repeat_interleave(sbf, repeats_sbf, 1)
     expanded_shf = _block_repeat(shf, block_size=block_size, repeats=[max_n] * max_l)
     shape = max_n * max_l
     if use_phi:
