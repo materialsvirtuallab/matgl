e462a8ab9ac270db8b9141d3976fe4392226cefd
lbluque <lbluque@berkeley.edu>
2023-09-19 15:03:56 -0700

    MAINT: use sequential in MLP

--- matgl/layers/_core.py
+++ matgl/layers/_core.py
@@ -22,26 +22,28 @@ class MLP(nn.Module):
         activate_last: bool = False,
         bias_last: bool = True,
     ) -> None:
-        """:param dims: Dimensions of each layer of MLP.
-        :param activation: Activation function.
-        :param activate_last: Whether to apply activation to last layer.
-        :param bias_last: Whether to apply bias to last layer.
+        """
+        Args:
+            dims: Dimensions of each layer of MLP.
+            activation: activation: Activation function.
+            activate_last: Whether to apply activation to last layer.
+            bias_last: Whether to apply bias to last layer.
         """
         super().__init__()
         self._depth = len(dims) - 1
-        self.layers = ModuleList()
+        self.layers = nn.Sequential()
 
         for i, (in_dim, out_dim) in enumerate(zip(dims[:-1], dims[1:])):
             if i < self._depth - 1:
                 self.layers.append(Linear(in_dim, out_dim, bias=True))
 
                 if activation is not None:
-                    self.layers.append(activation)  # type: ignore
+                    self.layers.append(activation)
             else:
                 self.layers.append(Linear(in_dim, out_dim, bias=bias_last))
 
                 if activation is not None and activate_last:
-                    self.layers.append(activation)  # type: ignore
+                    self.layers.append(activation)
 
     def __repr__(self):
         dims = []
@@ -86,11 +88,7 @@ class MLP(nn.Module):
         :param inputs: Input tensor
         :return: Output tensor
         """
-        x = inputs
-        for layer in self.layers:
-            x = layer(x)
-
-        return x
+        return self.layers(inputs)
 
 
 class GatedMLP(nn.Module):
