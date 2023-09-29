fix normalization inputs

matgl/models/_chgnet.py
@@ -80,7 +80,7 @@ class CHGNet(nn.Module, IOMixIn):
         pooling_operation: Literal["sum", "mean"] = "sum",
         readout_field: Literal["atom_feat", "bond_feat", "angle_feat"] = "atom_feat",
         activation_type: str = "swish",
-        conv_norm: Literal["graph"] | None = None,
+        normalization: Literal["graph"] | None = None,
         normalize_hidden: bool = False,
         is_intensive: bool = False,
         num_targets: int = 1,
@@ -119,7 +119,7 @@ class CHGNet(nn.Module, IOMixIn):
             pooling_operation: type of readout pooling operation to use.
             readout_field: field to readout from the graph.
             activation_type: activation function to use.
-            conv_norm: type of normalization to use in the convolution update blocks.
+            normalization: type of normalization to use in the convolution update blocks.
             normalize_hidden: whether to normalize the hidden layers in convolution update functions.
             is_intensive: whether the target is intensive or extensive.
             num_targets: number of targets to predict.
@@ -189,7 +189,7 @@ class CHGNet(nn.Module, IOMixIn):
                     conv_hidden_dims=atom_conv_hidden_dims,
                     edge_hidden_dims=bond_layer_hidden_dims,
                     activation=activation,
-                    normalization=conv_norm,
+                    normalization=normalization,
                     normalize_hidden=normalize_hidden,
                     num_state_feats=dim_state_embedding,
                     dropout=conv_dropout,
@@ -209,7 +209,7 @@ class CHGNet(nn.Module, IOMixIn):
                     bond_hidden_dims=bond_conv_hidden_dims,
                     angle_hidden_dims=angle_layer_hidden_dims,
                     activation=activation,
-                    normalization=conv_norm,
+                    normalization=normalization,
                     normalize_hidden=normalize_hidden,
                     bond_dropout=conv_dropout,
                     angle_dropout=conv_dropout,