0e1022d1073bdd92946e6d8bffc72848f3040c9d
Shyue Ping Ong <sp@ong.ai>
2023-11-27 13:00:08 -0800

    Add other models.

--- hubconf.py
+++ hubconf.py
@@ -14,5 +14,32 @@ def m3gnet_universal_potential(version="MP-2021.2.8-DIRECT", **kwargs):
         version (str): Defaults to "MP-2021.2.8-DIRECT". Other versions available.
         **kwargs: Pass through to matgl.load_model.
     """
-    # Call the model, load pretrained weights
     return matgl.load_model(f"M3GNet-{version}", **kwargs)
+
+
+def m3gnet_formation_energy(**kwargs):
+    """M3GNet Formation Energy Model.
+
+    Args:
+        version (str): Defaults to "MP-2018.6.1-EForm". Other versions available.
+        **kwargs: Pass through to matgl.load_model.
+    """
+    return matgl.load_model("M3GNet-MP-2018.6.1-Eform", **kwargs)
+
+
+def megnet_formation_energy(**kwargs):
+    """MEGNet Formation Energy Model.
+
+    Args:
+        **kwargs: Pass through to matgl.load_model.
+    """
+    return matgl.load_model("MEGNet-MP-2018.6.1-Eform", **kwargs)
+
+
+def megnet_band_gap_mfi(**kwargs):
+    """MEGNet Multi-fidelity Band Gap Model.
+
+    Args:
+        **kwargs: Pass through to matgl.load_model.
+    """
+    return matgl.load_model("MEGNet-MP-2019.4.1-BandGap-mfi", **kwargs)
