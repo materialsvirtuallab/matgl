Fix the sign in Hessian
Signed-off-by: Tsz Wai Ko <47970742+kenko911@users.noreply.github.com>

src/matgl/apps/pes.py
@@ -139,7 +139,7 @@ class Potential(nn.Module, IOMixIn):
             forces = -grads[0]
 
         if self.calc_hessian:
-            r = -grads[0].view(-1)
+            r = grads[0].view(-1)
             s = r.size(0)
             hessian = total_energies.new_zeros((s, s))
             for iatom in range(s):
@@ -153,8 +153,8 @@ class Potential(nn.Module, IOMixIn):
                 if matgl.float_th == torch.float16
                 else torch.abs(torch.det(lattice))
             )
-            sts = -grads[1]
-            scale = 1.0 / volume * -160.21766208
+            sts = grads[1]
+            scale = 1.0 / volume * 160.21766208
             sts = [i * j for i, j in zip(sts, scale, strict=False)] if sts.dim() == 3 else [sts * scale]  # type:ignore[assignment]
             stresses = torch.cat(sts)  # type:ignore[call-overload]
 