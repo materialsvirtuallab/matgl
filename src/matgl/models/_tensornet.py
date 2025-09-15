18d82d5ef17170d2995111f4be1a6781f1de84ef
kenko911 <kenko911@gmail.com>
2025-09-15 07:17:41 -0400

    fix ruff again

--- src/matgl/models/_tensornet.py
+++ src/matgl/models/_tensornet.py
@@ -187,9 +187,7 @@ class TensorNet(MatGLModel):
                 )
                 readout_feats = 2 * input_feats  # type: ignore
             elif readout_type == "weighted_atom":
-                self.readout = WeightedAtomReadOut(
-                    in_feats=input_feats, dims=[units, units], activation=activation
-                )  # type:ignore[assignment]
+                self.readout = WeightedAtomReadOut(in_feats=input_feats, dims=[units, units], activation=activation)
                 readout_feats = units + dim_state_feats if include_state else units  # type: ignore
             else:
                 self.readout = ReduceReadOut("mean", field=field)  # type: ignore
