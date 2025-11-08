c3d16d394318ff85e25da75f6c9d77c4c683ac05
Shyue Ping Ong <shyuep@gmail.com>
2025-11-08 09:06:17 -0800

    Fix pes.py.

--- src/matgl/apps/pes.py
+++ src/matgl/apps/pes.py
@@ -4,5 +4,15 @@ from __future__ import annotations
 
 from matgl.config import BACKEND
 
-if BACKEND == "dgl" or BACKEND == "pyg":
-    pass
+if BACKEND == "DGL":
+    try:
+        import dgl  # noqa
+    except ImportError as err:
+        raise ImportError("Please install DGL to use this backend.") from err
+    from ._pes_dgl import Potential
+else:
+    try:
+        import torch_geometric  # noqa
+    except ImportError as err:
+        raise ImportError("Please install torch_geometric to use this backend.") from err
+    from ._pes_pyg import Potential  # noqa
