b21eab465c9298f0c99db35c1e72c4a69c018047
kenko911 <kenko911@gmail.com>
2025-11-12 16:09:50 -0500

    pull again

--- src/matgl/config.py
+++ src/matgl/config.py
@@ -25,7 +25,7 @@ os.makedirs(MATGL_CACHE, exist_ok=True)
 PRETRAINED_MODELS_BASE_URL = "http://github.com/materialsvirtuallab/matgl/raw/main/pretrained_models/"
 
 # Set the backend. Note that not all models are available for all backends.
-BACKEND: Literal["PYG", "DGL"] = os.environ.get("MATGL_BACKEND", "DGL")  # type: ignore[assignment,return-value]
+BACKEND: Literal["PYG", "DGL"] = os.environ.get("MATGL_BACKEND", "PYG")  # type: ignore[assignment,return-value]
 
 
 if BACKEND == "DGL":
