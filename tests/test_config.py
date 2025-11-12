e9e7c28f9f6fea3575b23e9dca567b9166e4d553
Shyue Ping Ong <shyuep@gmail.com>
2025-11-12 12:34:00 -0800

    Update README.

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,9 +2,17 @@ from __future__ import annotations
 
 import os.path
 
+import pytest
+
+import matgl
 from matgl.config import MATGL_CACHE, clear_cache
 
 
 def test_clear_cache():
     clear_cache(False)
     assert not os.path.exists(MATGL_CACHE)
+
+
+def test_set_backend():
+    with pytest.raises(ValueError, match="Invalid backend"):
+        matgl.set_backend("nonsense")
