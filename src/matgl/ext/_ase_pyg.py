19deb39a195f61877943e277e24539e0ad995935
Mingjian Wen <wenxx151@gmail.com>
2025-11-30 15:03:59 +0800

    Update docs

--- src/matgl/ext/_ase_pyg.py
+++ src/matgl/ext/_ase_pyg.py
@@ -36,7 +36,6 @@ from matgl.graph._converters_pyg import GraphConverter
 if TYPE_CHECKING:
     from typing import Any
 
-    import torch
     from ase.optimize.optimize import Optimizer
     from torch_geometric.data import Data
 
@@ -156,6 +155,7 @@ class PESCalculator(Calculator):
             stress_unit (str): stress unit. Default: "GPa"
             stress_weight (float): conversion factor from GPa to eV/A^3, if it is set to 1.0, the unit is in GPa
             use_voigt (bool): whether the voigt notation is used for stress output
+            device: device (e.g. `cpu`, `cuda`) on which to evaluate the potential
             **kwargs: Kwargs pass through to super().__init__().
         """
         super().__init__(**kwargs)
